776686fe3987d7604ce2494f714df966ff2c7be9
akcase18 <aidan.case@casefamily.us>
2024-09-23 12:22:01 -0600

    Added some comments

--- src/threads.c
+++ src/threads.c
@@ -6,10 +6,10 @@
 #include <pico/multicore.h>
 #include <pico/cyw43_arch.h>
 
-#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+#define MAIN_TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
-#define SIDE_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+#define SIDE_TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)
 #define SIDE_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
 SemaphoreHandle_t semaphore;
@@ -19,35 +19,37 @@ int on;
 
 void side_thread(void *params)
 {
-	while (1) {
+    while (1)
+    {
         vTaskDelay(100);
         counter += counter + 1;
-		printf("hello world from %s! Count %d\n", "thread", counter);
-	}
+        printf("hello world from %s! Count %d\n", "thread", counter);
+    }
 }
 
 void main_thread(void *params)
 {
-	while (1) {
+    while (1)
+    {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
         vTaskDelay(100);
-		printf("hello world from %s! Count %d\n", "main", counter++);
+        printf("hello world from %s! Count %d\n", "main", counter++);
         on = !on;
-	}
+    }
 }
 
 int main(void)
 {
-    stdio_init_all();
-    hard_assert(cyw43_arch_init() == PICO_OK);
+    stdio_init_all();                          // Initialize all GPIO
+    hard_assert(cyw43_arch_init() == PICO_OK); // Ensure that on-board LED hardware is ok
     on = false;
     counter = 0;
     TaskHandle_t main, side;
-    semaphore = xSemaphoreCreateCounting(1, 1);
+    semaphore = xSemaphoreCreateCounting(1, 1); // Create semaphore
     xTaskCreate(main_thread, "MainThread",
                 MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &main);
     xTaskCreate(side_thread, "SideThread",
                 SIDE_TASK_STACK_SIZE, NULL, SIDE_TASK_PRIORITY, &side);
-    vTaskStartScheduler();
-	return 0;
+    vTaskStartScheduler(); // Start FreeRTOS
+    return 0;
 }
