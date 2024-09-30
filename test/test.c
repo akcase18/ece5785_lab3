349a1237c8b20a61a79e66c6d34dac293f8f7f70
akcase18 <aidan.case@casefamily.us>
2024-09-30 12:12:48 -0600

    Started work on testing

--- test/test.c
+++ test/test.c
@@ -3,33 +3,51 @@
 #include <stdint.h>
 #include <unity.h>
 #include "unity_config.h"
+#include "lab3.h"
+#include <FreeRTOS.h>
+#include <semphr.h>
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_variable_assignment()
+/**
+ * Should increment the counter every time 
+ */
+void test_side_thread()
 {
-    int x = 1;
-    TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
+    int counter = 0;
+    SemaphoreHandle_t semaphore;
+    TEST_ASSERT_TRUE(side_thread_logic(counter, semaphore) == 1);
 }
 
-void test_multiplication(void)
+void test_side_thread_locked()
 {
-    int x = 30;
-    int y = 6;
-    int z = x / y;
-    TEST_ASSERT_TRUE_MESSAGE(z == 5, "Multiplication of two integers returned incorrect value.");
+
+}
+
+void test_main_thread()
+{
+    int counter = 0;
+    SemaphoreHandle_t semaphore;
+    TEST_ASSERT_TRUE(main_thread_logic(counter, semaphore) == 1);
+}
+
+void test_main_thread_locked()
+{
+
 }
 
 int main (void)
 {
     stdio_init_all();
-    sleep_ms(5000); // Give time for TTY to attach.
-    printf("Start tests\n");
+    // sleep_ms(5000); // Give time for TTY to attach.
+    // printf("Start tests\n");
     UNITY_BEGIN();
-    RUN_TEST(test_variable_assignment);
-    RUN_TEST(test_multiplication);
-    sleep_ms(5000);
+    RUN_TEST(test_side_thread);
+    RUN_TEST(test_side_thread_locked);
+    RUN_TEST(test_main_thread);
+    RUN_TEST(test_main_thread_locked);
+    // sleep_ms(5000);
     return UNITY_END();
 }
