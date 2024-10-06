f91f02bbc048b3b91368fd4ab6b2cc20d4ff9459
akcase18 <aidan.case@casefamily.us>
2024-10-06 17:34:27 -0600

    Changed functions to return the result of taking a semaphore and uses pointers to reference counter

--- src/lab3.c
+++ src/lab3.c
@@ -1,24 +1,28 @@
 #include "lab3.h"
 
-int side_thread_logic(int counter, SemaphoreHandle_t semaphore)
+int side_thread_logic(int *counter, SemaphoreHandle_t semaphore)
 {
-    xSemaphoreTake(semaphore, 0);
+    if (xSemaphoreTake(semaphore, 100) == pdFALSE)
     {
-        counter = counter + 1;                                       // Adding counter + 1 to counter
-        printf("hello world from %s! Count %d\n", "thread", counter); // Print out thread and the counter value
+        return pdFALSE;
+    }
+    {
+        *counter = *counter + 1;                                       // Adding counter + 1 to counter
+        printf("hello world from %s! Count %d\n", "thread", *counter); // Print out thread and the counter value
     }
     xSemaphoreGive(semaphore);
-
-    return counter;
+    return pdTRUE;
 }
 
-int main_thread_logic(int counter, SemaphoreHandle_t semaphore)
+int main_thread_logic(int *counter, SemaphoreHandle_t semaphore)
 {
-    xSemaphoreTake(semaphore, 0);
+    if (xSemaphoreTake(semaphore, 0) == pdFALSE);
     {
-        printf("hello world from %s! Count %d\n", "main", counter++); // Print out main and increment and print out counter
+        return pdFALSE;
     }
-    xSemaphoreGive(semaphore);
-
-    return counter;
+    {
+        printf("hello world from %s! Count %d\n", "main", (*counter)++); // Print out main and increment and print out counter
+    }
+    SemaphoreGive(semaphore);
+    return pdTRUE;
 }
